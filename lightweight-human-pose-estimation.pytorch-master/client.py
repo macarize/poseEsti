3e226ec855997f64c338cec966295118b1daf6a4
macarize <54901021+macarize@users.noreply.github.com>
2020-08-27 20:11:23 +0900

    Initial commit

---
+++ lightweight-human-pose-estimation.pytorch-master/client.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import socket
+import sys
+import pickle
+import struct
+
+cap=cv2.VideoCapture(0)
+clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect(('localhost',8089))
+
+while True:
+    ret,frame=cap.read()
+    # Serialize frame
+    data = pickle.dumps(frame)
+
+    # Send message length first
+    message_size = struct.pack("L", len(data))
+
+    # Then data
+    clientsocket.sendall(message_size + data)
+
+    pose = clientsocket.recv(4096)
+    pose = pickle.loads(pose)
+    print(pose.keypoints)
+
---
+++ lightweight-human-pose-estimation.pytorch-master/server.py
@@ -0,0 +1,63 @@
+import pickle
+import socket
+import struct
+import torch
+import cv2
+import os
+
+from test import run_demo, ImageReader
+from models.with_mobilenet import PoseEstimationWithMobileNet
+from modules.load_state import load_state
+
+HOST = ''
+PORT = 8089
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+
+s.bind((HOST, PORT))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+
+conn, addr = s.accept()
+
+data = b'' ### CHANGED
+payload_size = struct.calcsize("L") ### CHANGED
+
+net = PoseEstimationWithMobileNet()
+checkpoint = torch.load('checkpoint_iter_370000.pth', map_location='cpu')
+load_state(net, checkpoint)
+
+while True:
+
+    # Retrieve message size
+    while len(data) < payload_size:
+        data += conn.recv(4096)
+
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
+
+    # Retrieve all data based on message size
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    # Extract frame
+    frame = pickle.loads(frame_data)
+
+    pose = run_demo(net, frame, 256, 1, 0, 1)
+    if pose is not None:
+        #print(pose.keypoints)
+        print('you')
+
+    else:
+        print("no keeypoints detected")
+
+    pose = pickle.dumps(pose)
+    conn.sendall(pose)
+
+
