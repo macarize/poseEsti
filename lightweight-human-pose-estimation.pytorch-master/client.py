0de20e2d471976676e066cb7ddd2f3d6e2bb07c0
macarize <54901021+macarize@users.noreply.github.com>
2020-08-31 15:20:55 +0900

    asdf
    asdf

--- lightweight-human-pose-estimation.pytorch-master/client.py
+++ lightweight-human-pose-estimation.pytorch-master/client.py
@@ -5,13 +5,15 @@ import sys
 import pickle
 import struct
 
-cap=cv2.VideoCapture(0)
+#cap=cv2.VideoCapture(0)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 clientsocket.connect(('54.180.176.99',8089))
-
+cap = cv2.VideoCapture('squat.mp4')
 while True:
     ret,frame=cap.read()
     # Serialize frame
+    frame = cv2.transpose(frame)
+    frame = cv2.flip(frame,flipCode=1)
     data = pickle.dumps(frame)
     # Send message length first
     message_size = struct.pack("=L", len(data))
@@ -21,7 +23,7 @@ while True:
 
     pose = clientsocket.recv(4096)
     pose = pickle.loads(pose)
-    print(pose.keypoints)
+    print(pose.keypoints[8][0] + " " + pose.keypoints[11][0])
 
     pose.draw(frame)
     cv2.imshow('test', frame)
--- lightweight-human-pose-estimation.pytorch-master/server.py
+++ lightweight-human-pose-estimation.pytorch-master/server.py
@@ -27,10 +27,11 @@ net = PoseEstimationWithMobileNet()
 checkpoint = torch.load('checkpoint_iter_370000.pth', map_location='cpu')
 load_state(net, checkpoint)
 
-conn, addr = s.accept()
+
 print('ACCENPTED')
 while True:
 
+    conn, addr = s.accept()
     # Retrieve message size
     while len(data) < payload_size:
         data += conn.recv(4096)
@@ -62,5 +63,4 @@ while True:
 
     else:
         print("no keypoints detected")
-        continue
 
