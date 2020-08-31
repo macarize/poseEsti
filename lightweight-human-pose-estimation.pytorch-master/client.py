bbf8ad99dec99adbef794e60395581eaf07fe4c2
macarize <54901021+macarize@users.noreply.github.com>
2020-08-31 18:17:07 +0900

    adffadsf
    fadsdsf

--- lightweight-human-pose-estimation.pytorch-master/client.py
+++ lightweight-human-pose-estimation.pytorch-master/client.py
@@ -5,15 +5,28 @@ import sys
 import pickle
 import struct
 
-#cap=cv2.VideoCapture(0)
+stepA = False
+stepB = False
+count = 0
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (50, 400)
+topLeft = (150, 400)
+
+fontScale = 3
+fontColor = (255, 0, 0)
+lineType = 2
+
+
+cap=cv2.VideoCapture(0)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 clientsocket.connect(('54.180.176.99',8089))
-cap = cv2.VideoCapture('squat.mp4')
+#cap = cv2.VideoCapture('squat.mp4')
 while True:
     ret,frame=cap.read()
     # Serialize frame
-    frame = cv2.transpose(frame)
-    frame = cv2.flip(frame,flipCode=1)
+    #frame = cv2.transpose(frame)
+    #frame = cv2.flip(frame,flipCode=1)
     data = pickle.dumps(frame)
     # Send message length first
     message_size = struct.pack("=L", len(data))
@@ -23,9 +36,54 @@ while True:
 
     pose = clientsocket.recv(4096)
     pose = pickle.loads(pose)
-    print(pose.keypoints[8][0], pose.keypoints[11][0])
 
     pose.draw(frame)
     cv2.imshow('test', frame)
     if cv2.waitKey(1) == ord('q'):
        break
+
+
+
+    A = np.array([pose.keypoints[8][0],pose.keypoints[8][1]])
+    B = np.array([pose.keypoints[9][0],pose.keypoints[9][1]])
+    C = np.array([pose.keypoints[10][0],pose.keypoints[10][1]])
+
+    BA = A - B
+    BC = C - B
+
+    cosine_angle = np.dot(BA, BC) / (np.linalg.norm(BA) * np.linalg.norm(BC))
+    angle = np.arccos(cosine_angle)
+    angle = np.degrees(angle)
+    print(angle)
+
+    if angle > 140 :
+        stepA = True
+    if angle < 70 :
+        stepB = True
+
+    if stepA and stepB is True :
+        if angle > 140:
+            stepA = False
+            stepB = False
+            count += 1
+
+            cv2.putText(frame,"good",
+                        topLeft,
+                        font,
+                        fontScale,
+                        fontColor,
+                        lineType)
+    if stepA is True and stepB is False :
+        if angle > 140:
+            stepA = False
+            stepB = False
+
+    cv2.putText(frame, str(count),
+                bottomLeftCornerOfText,
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+    cv2.imshow("img", frame)
+
+    print(count)
