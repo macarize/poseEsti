32493068abe035a9ec6af5edcbe3c945b127da4c
macarize <54901021+macarize@users.noreply.github.com>
2020-10-18 17:28:51 +0900

    dsdsf
    dsfdsfdsfdsf

--- lightweight-human-pose-estimation.pytorch-master/app.py
+++ lightweight-human-pose-estimation.pytorch-master/app.py
@@ -111,86 +111,87 @@ def gen():
 
         pose = run_demo(net, frame, 256, 0, 0, 1)
 
-        pose.draw(frame)
-        #cv2.imshow('test', frame)
-        if cv2.waitKey(1) == ord('q'):
-            break
+        if pose is not None:
+            pose.draw(frame)
+            #cv2.imshow('test', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
 
-        A = np.array([pose.keypoints[8][0], pose.keypoints[8][1]])
-        B = np.array([pose.keypoints[9][0], pose.keypoints[9][1]])
-        C = np.array([pose.keypoints[10][0], pose.keypoints[10][1]])
+            A = np.array([pose.keypoints[8][0], pose.keypoints[8][1]])
+            B = np.array([pose.keypoints[9][0], pose.keypoints[9][1]])
+            C = np.array([pose.keypoints[10][0], pose.keypoints[10][1]])
+    
+            BA = A - B
+            BC = C - B
 
-        BA = A - B
-        BC = C - B
+            cosine_angle = np.dot(BA, BC) / (np.linalg.norm(BA) * np.linalg.norm(BC))
+            angle = np.arccos(cosine_angle)
+            angle = np.degrees(angle)
+            #print(angle)
 
-        cosine_angle = np.dot(BA, BC) / (np.linalg.norm(BA) * np.linalg.norm(BC))
-        angle = np.arccos(cosine_angle)
-        angle = np.degrees(angle)
-        #print(angle)
-
-        if angle > 140:
-            stepA = True
-            if stdupAngle is 0:
-                stdupAngle = angle
-            if angle > stdupAngle:
-                stdupAngle = angle
-        if angle < 70:
-            stepB = True
-            if sitAngle is 0:
-                sitAngle = angle
-            if angle < sitAngle:
-                sitAngle = angle
-
-        if stepA and stepB is True:
             if angle > 140:
-                stepA = False
-                stepB = False
-                count += 1
+                stepA = True
+                if stdupAngle is 0:
+                    stdupAngle = angle
+                if angle > stdupAngle:
+                    stdupAngle = angle
+            if angle < 70:
+                stepB = True
+                if sitAngle is 0:
+                    sitAngle = angle
+                if angle < sitAngle:
+                    sitAngle = angle
 
-                if sitAngle > 60:
-                    cv2.putText(frame, "Bend your knee more",
-                                topLeft,
+            if stepA and stepB is True:
+                if angle > 140:
+                    stepA = False
+                    stepB = False
+                    count += 1
+
+                    if sitAngle > 60:
+                        cv2.putText(frame, "Bend your knee more",
+                                    topLeft,
+                                    font,
+                                    fontScale,
+                                    fontColor,
+                                    lineType)
+
+                    stdupDiff = 140 - stdupAngle
+                    sitDiff = 70 - sitAngle
+
+                    stdupDiff = abs(stdupDiff)
+                    sitDiff = abs(sitDiff)
+                    correctness = 280 - (stdupDiff + sitDiff)
+
+                    cv2.putText(frame, "correctness" + str(correctness),
+                                bottomLeftCornerOfText,
                                 font,
                                 fontScale,
                                 fontColor,
                                 lineType)
 
-                stdupDiff = 140 - stdupAngle
-                sitDiff = 70 - sitAngle
+                    sitAngle = 0
+                    stdupAngle = 0
+            if stepA is True and stepB is False:
+                if angle > 140:
+                    stepA = False
+                    stepB = False
+                    sitAngle = 0
+                    stdupAngle = 0
 
-                stdupDiff = abs(stdupDiff)
-                sitDiff = abs(sitDiff)
-                correctness = 280 - (stdupDiff + sitDiff)
+            cv2.putText(frame, "count" + str(count),
+                        bottomLeftCornerOfText,
+                        font,
+                        fontScale,
+                        fontColor,
+                        lineType)
+            #cv2.imshow("img", frame)
 
-                cv2.putText(frame, "correctness" + str(correctness),
-                            bottomLeftCornerOfText,
-                            font,
-                            fontScale,
-                            fontColor,
-                            lineType)
-
-                sitAngle = 0
-                stdupAngle = 0
-        if stepA is True and stepB is False:
-            if angle > 140:
-                stepA = False
-                stepB = False
-                sitAngle = 0
-                stdupAngle = 0
-
-        cv2.putText(frame, "count" + str(count),
-                    bottomLeftCornerOfText,
-                    font,
-                    fontScale,
-                    fontColor,
-                    lineType)
-        #cv2.imshow("img", frame)
-
-        print(count)
-        encode_return_code, image_buffer = cv2.imencode('.jpg', frame)
-        io_buf = io.BytesIO(image_buffer)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + io_buf.read() + b'\r\n')
+            print(count)
+            encode_return_code, image_buffer = cv2.imencode('.jpg', frame)
+            io_buf = io.BytesIO(image_buffer)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + io_buf.read() + b'\r\n')
 
 
 def gen2():
@@ -263,7 +264,8 @@ def gen2():
 
         pose = run_demo(net, frame, 256, 0, 0, 1)
 
-        pose.draw(frame)
+        if pose is not None:
+            pose.draw(frame)
         #cv2.imshow('test', frame)
         if cv2.waitKey(1) == ord('q'):
             break
