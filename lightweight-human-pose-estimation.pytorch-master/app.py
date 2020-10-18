4d3d91702007b76c217cdc2c2b589d2cd2be34c0
macarize <54901021+macarize@users.noreply.github.com>
2020-10-18 15:11:58 +0900

    adfdafadf
    fadfadfda

--- lightweight-human-pose-estimation.pytorch-master/app.py
+++ lightweight-human-pose-estimation.pytorch-master/app.py
@@ -2,9 +2,7 @@
 from flask import Flask, render_template, Response
 import io
 import cv2
-from flask import Flask
-from flask_cors import CORS
-from flask import request
+
 import pickle
 import socket
 import struct
@@ -15,7 +13,6 @@ import os
 from test import run_demo, ImageReader
 from models.with_mobilenet import PoseEstimationWithMobileNet
 from modules.load_state import load_state
-from flask import request
 
 import cv2
 import numpy as np
@@ -27,12 +24,14 @@ from flask import Flask, render_template, Response
 import io
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/')
-def index():
+@app.route('/<type>')
+def index(type):
     """Video streaming home page."""
-    return render_template('index.html')
+    if type == 'squat'
+        return render_template('index.html')
+    elif type == 'pushup'
+        return render_template('index2.html')
 
 
 def gen():
@@ -185,7 +184,8 @@ def gen():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + io_buf.read() + b'\r\n')
 
-def gen():
+
+def gen2():
     """Video streaming generator function."""
     HOST = ''
     PORT = 8088
@@ -211,12 +211,14 @@ def gen():
     data = b''  ### CHANGED
     payload_size = struct.calcsize("=L")  ### CHANGED
 
+    print('pose')
+    A = 0
+    B = 0
+    C = 0
+
     stepA = False
     stepB = False
     count = 0
-
-    sitAngle = 0
-    stdupAngle = 0
 
     font = cv2.FONT_HERSHEY_SIMPLEX
     bottomLeftCornerOfText = (50, 400)
@@ -226,42 +228,35 @@ def gen():
     fontColor = (255, 0, 0)
     lineType = 2
 
-    emptyPoses = []
-
+    #cap = cv2.VideoCapture(0)
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect(('54.180.176.99', 8089))
+    # cap = cv2.VideoCapture('pushUp.mp4')
     while True:
-        while len(data) < payload_size:
-            data += conn.recv(4096)
-        print('MESSAGESIZE')
-        print(payload_size)
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("=L", packed_msg_size)[0]  ### CHANGED
-        print('unpack')
-        # Retrieve all data based on message size
-        while len(data) < msg_size:
-            data += conn.recv(4096)
-            print(len(data))
-            print(msg_size)
-        print('RECIEVED')
+        ret, frame = cap.read()
+        # Serialize frame
+        # frame = cv2.transpose(frame)
+        # frame = cv2.flip(frame,flipCode=1)
+        data = pickle.dumps(frame)
+        # Send message length first
+        message_size = struct.pack("=L", len(data))
 
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
+        # Then data
+        clientsocket.sendall(message_size + data)
 
-        # Extract frame
-        frame = pickle.loads(frame_data)
-
-        #read_return_code, frame = vc.read()
-
-        pose = run_demo(net, frame, 256, 0, 0, 1)
+        pose = clientsocket.recv(4096)
+        pose = pickle.loads(pose)
 
         pose.draw(frame)
         #cv2.imshow('test', frame)
         if cv2.waitKey(1) == ord('q'):
             break
 
-        A = np.array([pose.keypoints[8][0], pose.keypoints[8][1]])
-        B = np.array([pose.keypoints[9][0], pose.keypoints[9][1]])
-        C = np.array([pose.keypoints[10][0], pose.keypoints[10][1]])
+        A = np.array([pose.keypoints[2][0], pose.keypoints[2][1]])
+        if B is 0:
+            B = np.array([pose.keypoints[10][0], pose.keypoints[10][1]])
+        if C is 0:
+            C = np.array([pose.keypoints[4][0], pose.keypoints[4][1]])
 
         BA = A - B
         BC = C - B
@@ -271,70 +266,40 @@ def gen():
         angle = np.degrees(angle)
         print(angle)
 
-        if angle > 140:
+        if angle > 25:
             stepA = True
-            if stdupAngle is 0:
-                stdupAngle = angle
-            if angle > stdupAngle:
-                stdupAngle = angle
-        if angle < 70:
+        if angle < 15:
             stepB = True
-            if sitAngle is 0:
-                sitAngle = angle
-            if angle < sitAngle:
-                sitAngle = angle
 
         if stepA and stepB is True:
-            if angle > 140:
+            if angle > 25:
                 stepA = False
                 stepB = False
                 count += 1
 
-                if sitAngle > 60:
-                    cv2.putText(frame, "Bend your knee more",
-                                topLeft,
-                                font,
-                                fontScale,
-                                fontColor,
-                                lineType)
-
-                stdupDiff = 140 - stdupAngle
-                sitDiff = 70 - sitAngle
-
-                stdupDiff = abs(stdupDiff)
-                sitDiff = abs(sitDiff)
-                correctness = 280 - (stdupDiff + sitDiff)
-
-                cv2.putText(frame, "correctness" + str(correctness),
-                            bottomLeftCornerOfText,
+                cv2.putText(frame, "good",
+                            topLeft,
                             font,
                             fontScale,
                             fontColor,
                             lineType)
-
-                sitAngle = 0
-                stdupAngle = 0
         if stepA is True and stepB is False:
-            if angle > 140:
+            if angle > 25:
                 stepA = False
                 stepB = False
-                sitAngle = 0
-                stdupAngle = 0
-
         cv2.putText(frame, "count" + str(count),
                     bottomLeftCornerOfText,
                     font,
                     fontScale,
                     fontColor,
                     lineType)
-        #cv2.imshow("img", frame)
+        cv2.imshow("img", frame)
 
-        #print(count)
+        print(count)
         encode_return_code, image_buffer = cv2.imencode('.jpg', frame)
         io_buf = io.BytesIO(image_buffer)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + io_buf.read() + b'\r\n')
-
 
 @app.route('/video_feed')
 def video_feed():
@@ -342,6 +307,16 @@ def video_feed():
     return Response(
         gen(),
         mimetype='multipart/x-mixed-replace; boundary=frame'
+
+    )
+
+@app.route('/video_feed2')
+def video_feed2():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(
+        gen(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+
     )
 
 if __name__ == '__main__':
